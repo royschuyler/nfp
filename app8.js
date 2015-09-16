55a8ef9118c4ed3f7146f2470b14186b57602c28
royschuyler <royschuyler@yahoo.com>
2015-09-16 10:25:46 -0500

    cleaned up table, added more dummy data to db

--- app8.js
+++ app8.js
@@ -4,14 +4,25 @@ var app = express();
 var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database('Tester3');
 
-
-
 app.set('view engine', 'jade');
-
 
 app.get('/', function (req, res) {
 
    db.serialize(function() {
+
+
+    db.run('DROP TABLE Donor');
+
+    db.run('CREATE TABLE Donor (PrimaryID integer primary key not null, FirstName text not null, LastName text not null, email text not null)');
+    db.run('INSERT INTO Donor(PrimaryID, FirstName, LastName, email) VALUES(1, "John", "Smithmun", "abc@mail.com")');
+    db.run('INSERT INTO Donor(FirstName, LastName, email) VALUES("Tim", "Brahmstein", "jebadiah@amish.com")');
+    db.run('INSERT INTO Donor(FirstName, LastName, email) VALUES("Phil", "Resurrected", "philmun@phlaker.com")');
+    db.run('INSERT INTO Donor(FirstName, LastName, email) VALUES("Greg", "Phaulstein", "maude@lebski.com")');
+
+
+
+
+
     db.all('SELECT * FROM Donor', function(err, rows) {
       var arr = [];
       console.log(rows);
