c19c02af855f23fe7fe64a5ea7b14f975e62b009
royschuyler <royschuyler@yahoo.com>
2015-09-17 18:10:49 -0500

    form posts to db

--- app8.js
+++ app8.js
@@ -17,13 +17,13 @@ app.get('/', function (req, res) {
 
    db.serialize(function() {
 
-    db.run('DROP TABLE Donor');
+    // db.run('DROP TABLE Donor');
 
-    db.run('CREATE TABLE Donor (PrimaryID integer primary key not null, FirstName text not null, LastName text not null, email text not null)');
-    db.run('INSERT INTO Donor(PrimaryID, FirstName, LastName, email) VALUES(1, "John", "Smithmun", "abc@mail.com")');
-    db.run('INSERT INTO Donor(FirstName, LastName, email) VALUES("Tim", "Brahmstein", "jebadiah@amish.com")');
-    db.run('INSERT INTO Donor(FirstName, LastName, email) VALUES("Phil", "Resurrected", "philmun@phlaker.com")');
-    db.run('INSERT INTO Donor(FirstName, LastName, email) VALUES("Greg", "Phaulstein", "maude@lebski.com")');
+    // db.run('CREATE TABLE Donor (PrimaryID integer primary key not null, FirstName text not null, LastName text not null, email text not null)');
+    // db.run('INSERT INTO Donor(PrimaryID, FirstName, LastName, email) VALUES(1, "John", "Smithmun", "abc@mail.com")');
+    // db.run('INSERT INTO Donor(FirstName, LastName, email) VALUES("Tim", "Brahmstein", "jebadiah@amish.com")');
+    // db.run('INSERT INTO Donor(FirstName, LastName, email) VALUES("Phil", "Resurrected", "philmun@phlaker.com")');
+    // db.run('INSERT INTO Donor(FirstName, LastName, email) VALUES("Greg", "Phaulstein", "maude@lebski.com")');
 
     db.all('SELECT * FROM Donor', function(err, rows) {
       var arr = [];
@@ -37,7 +37,15 @@ app.get('/', function (req, res) {
 app.all('/signup', function(req,res){
   res.render('signup')
   console.log(req.body);
-})
+  console.log(req.body.lname)
+   db.serialize(function() {
+
+
+     db.run('INSERT INTO Donor(FirstName, LastName, email) VALUES(' + "'" + req.body.fname + "'" + ',' + "'" + req.body.lname + "'" + ',' + "'" + req.body.email + "'" + ')');
+
+
+});
+});
 
 app.listen(3000);
 
