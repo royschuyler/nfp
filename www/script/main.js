2ad7c9f67164fedbaee28ffbf2b5769716b50b0d
royschuyler <royschuyler@yahoo.com>
2015-09-20 15:09:35 -0500

    got a test click event working client side

--- www/script/main.js
+++ www/script/main.js
@@ -1,1 +1,16 @@
-console.log('hello')
+console.log('hello');
+var test = "yaaa"
+
+var btn = $('btn');
+var body = $('body');
+
+// body.css('background-color',"")
+
+
+$(document).ready(function(){
+  $('.test').on('click', function(e) {
+    e.preventDefault()
+    console.log('heyy')
+  });
+});
+
